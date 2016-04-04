5f15afd111b58824f62b088debb14cc946a74b70
mugithesathish <mugithe.sathish@gmail.com>
2016-04-04 22:39:51 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Purpose = Backup of Important Data
+#Created on 04-04-2016
+#Author = Sourav Bansal
+#Version 1.0
+#START
+
+TIME=`date +%b-%d-%y`            # This Command will add date in Backup File Name.
+FILENAME=backup-$TIME.tar.gz     # Here i define Backup file name format.
+SRCDIR=/imp-data                 # Location of Important Data Directory (Source of backup).
+DESDIR=/mybackupfolder           # Destination of backup file.
+tar -cpzf $DESDIR/$FILENAME $SRCDIR
+
+#END
